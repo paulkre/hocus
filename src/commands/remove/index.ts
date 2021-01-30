ff40651c1596bea1205e7a18aeb07084d4da4019
Paul Kretschel <paul.kretschel@icloud.com>
2021-01-30 22:41:43 +0100

    Add default action for "remove" command

--- src/commands/remove/index.ts
+++ src/commands/remove/index.ts
@@ -1,7 +1,8 @@
 import { createCommand } from "../../command";
 import * as style from "../../style";
+import { logError } from "../../utils";
 import { createRemoveProjectCommand } from "./project";
-import { createRemoveSessionCommand } from "./session";
+import { createRemoveSessionCommand, removeSession } from "./session";
 
 export function createRemoveCommand() {
   return createCommand("remove")
@@ -9,5 +10,9 @@ export function createRemoveCommand() {
       `Remove a ${style.bold("session")} or a ${style.bold("project")}`
     )
     .addCommand(createRemoveProjectCommand())
-    .addCommand(createRemoveSessionCommand());
+    .addCommand(createRemoveSessionCommand())
+    .action(async () => {
+      const result = await removeSession();
+      if (result.err) logError(result.val);
+    });
 }
--- src/commands/remove/session.ts
+++ src/commands/remove/session.ts
@@ -1,30 +1,36 @@
+import { Ok, Result } from "ts-results";
 import { createCommand } from "../../command";
 import { deleteSession } from "../../data/sessions";
 import * as style from "../../style";
 import { logError } from "../../utils";
 import { resolveSession } from "../../resolve/session";
 
+export async function removeSession(
+  id?: string
+): Promise<Result<void, string>> {
+  const resolveResult = await resolveSession(id);
+  if (resolveResult.err) return resolveResult;
+
+  const session = resolveResult.val;
+  if (!session) {
+    console.log(`Session could not be found.`);
+    return Ok(undefined);
+  }
+
+  const deleteResult = await deleteSession(session);
+  if (deleteResult.err) return deleteResult;
+
+  console.log(`Session ${style.bold(session.id)} was removed successfully.`);
+
+  return Ok(undefined);
+}
+
 export function createRemoveSessionCommand() {
   return createCommand("session")
     .arguments("[id]")
     .description(`Remove a ${style.bold("session")}`)
     .action(async (id: string | undefined) => {
-      const resolveResult = await resolveSession(id);
-      if (resolveResult.err) {
-        logError(resolveResult.val);
-        return;
-      }
-
-      const session = resolveResult.val;
-      if (!session) {
-        console.log(`Session could not be found.`);
-        return;
-      }
-
-      await deleteSession(session);
-
-      console.log(
-        `Session ${style.bold(session.id)} was removed successfully.`
-      );
+      const result = await removeSession(id);
+      if (result.err) logError(result.val);
     });
 }
