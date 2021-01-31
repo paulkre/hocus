be5c7300989c0343048d3f833e4eeaab66b5ba9a
Paul Kretschel <paul.kretschel@icloud.com>
2021-01-31 03:01:22 +0100

    Fix definition of tags in add command

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -9,17 +9,25 @@ import { logError } from "../utils";
 export function createAddCommand() {
   return createCommand("add")
     .arguments("[project] [start] [end]")
+    .option(
+      "-t, --tags <tags...>",
+      `The ${style.bold("Tags")} to be used on the added ${style.bold(
+        "session"
+      )} (comma or space separated)`
+    )
     .description(`Add a new ${style.bold("session")} with the given attributes`)
     .action(
       async (
         projectName: string | undefined,
         start: string | undefined,
-        end: string | undefined
+        end: string | undefined,
+        { tags }: { tags?: string[] }
       ) => {
         const input = await inquireSessionData({
           projectName,
           start,
           end,
+          tags: projectName && start && end && !tags ? [] : tags,
         });
 
         const sessionDataParseResult = await parseSessionData(input);
